c58c02f4dd1c27ee5d453c4511c77a844f023c65
ceciliaglemos <ceciliaglemos@gmail.com>
2022-09-21 18:26:08 -0300

    Exercício repetição

---
+++ guanabara/exercicios/ex017/ambiente.js
@@ -0,0 +1,28 @@
+
+for (let c=1;c<=10;c++) {
+    console.log(`Passo ${c}`);
+}
+
+
+/* 
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 9) */
+
+/* while (c<= 9){
+    console.log (`Passo ${c}`)
+} */
+
+/* 
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+console.log (`Passo ${c}`)
+*/
---
+++ guanabara/exercicios/ex017/dep.js
@@ -0,0 +1,5 @@
+console.log('vai começar...')
+for(let c=1;c<=4;c++) {
+    console.log(c)
+}
+console.log('FIM!');
